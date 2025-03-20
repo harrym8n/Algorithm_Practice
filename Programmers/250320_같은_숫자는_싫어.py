ca3557262f2f863e661a848890ac12a417b2cb39
harrym8n <harrym8n@gmail.com>
2025-03-20 22:38:27 +0900

    Solve quiz1 on 250320

---
+++ Programmers/250320_같은_숫자는_싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)):
+        if i == (len(arr)-1):
+            answer.append(arr[i])
+        else:
+            if arr[i] != arr[i+1]:
+                answer.append(arr[i])
+    return answer
